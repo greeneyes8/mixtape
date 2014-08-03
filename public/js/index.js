916a5fdf8de2cf85ea8b1b35bc834e7fad89831b
Andy Jiang <andyjiang@gmail.com>
2014-08-03 11:30:19 -0700

    added button listener

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,18 @@
 // Bind click to connect-soundcloud.
 
-// initialize client with app credentials
+function init () {
+  var button = document.getElementById('connect-soundcloud');
+  button.onclick = function (e) {
+    // initialize client with app credentials
+    SC.connect(function() {
+      SC.get('/me', function(me) {
+        alert('Hello, ' + me.username);
+      });
+    });
+  };
+}
+
 SC.initialize({
   client_id: '0482da9ad987e87ba383f481c357f403',
   redirect_uri: 'http://mixtape93.herokuapp.com/api/oauth'
-});
-
-// initiate auth popup
-SC.connect(function() {
-  SC.get('/me', function(me) {
-    alert('Hello, ' + me.username);
-  });
 });
