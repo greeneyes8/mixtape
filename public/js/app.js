7ccb82c8a489b6fe7cd193991b4445921ea8a79b
Andy Jiang <andyjiang@gmail.com>
2014-08-18 15:04:41 -0700

    added angular

---
+++ public/js/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+// Declare app level module which depends on services, etc. ====================
+var mixtape93 = angular.module('mixtape93', []);
---
+++ public/js/controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+mixtape93.controller('mainController',
+  ['$scope', '$http',
+  function ($scope, $http)
+{
+  SC.initialize({
+    client_id: '0482da9ad987e87ba383f481c357f403',
+    redirect_uri: 'http://mixtape93.herokuapp.com/api/oauth'
+  });
+  $scope.authenticateToSoundcloud = function authenticateToSoundcloud () {
+    // initialize client with app credentials
+    SC.connect(function() {
+
+      // SC.get('/me', function(me) {
+      //   alert('Hello, ' + me.username);
+      // });
+    });
+  };
+}]);
