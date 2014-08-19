fc0f3aaa29f44d6bebeb6ba992c14a413548a05c
Andy Jiang <andyjiang@gmail.com>
2014-08-19 00:42:01 -0700

    this is it

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -40,9 +40,8 @@ mixtape93.controller('streamController',
 
   SC.get('/me/activities/all', function (activities) {
     // console.log(JSON.stringify(activities,null,2));
-    stream.concat.apply(stream, activities.collection);
+    $scope.stream = stream.concat.apply(stream, activities.collection);
     $scope.$apply();
-
     console.log(stream);
   });
 }]);
