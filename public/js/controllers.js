97bafbc43ff8b5bdb5bf9194c8f09d96df56912e
Andy Jiang <andyjiang@gmail.com>
2014-08-19 11:51:18 -0700

    so unperformant

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,7 +6,7 @@ mixtape93.controller('authController',
 {
   SC.initialize({
     client_id: '0482da9ad987e87ba383f481c357f403',
-    redirect_uri: 'http://mixtape93.herokuapp.com/api/oauth'
+    redirect_uri: 'http://6cc005fa.ngrok.com/api/oauth'
   });
   $scope.authenticateToSoundcloud = function authenticateToSoundcloud () {
     SC.connect(function () {
@@ -22,22 +22,24 @@ mixtape93.controller('streamController',
   function ($scope, $http, $sce)
 {
   // Initialize variables.
-  var stream = $scope.sc.stream = [];
+  $scope.sc = {
+    stream: [],
+    getEmbed: function getEmbed () {
+      for (var i = 0; i < $scope.sc.stream.length; i++) {
+        var uri = $scope.sc.stream[i].origin.uri;
+        SC.oEmbed(uri, { auto_play: false }, function(oEmbed) {
+          this.origin.oEmbed = oEmbed;
+          this.origin.oEmbed.html = $sce.trustAsHtml(oEmbed.html);
+          $scope.$apply();
+        }.bind($scope.sc.stream[i]));
+      }
+      console.log($scope.sc.stream);
+    }
+  };
+
   SC.get('/me/activities/all', function (activities) {
     // console.log(JSON.stringify(activities,null,2));
     $scope.sc.stream = $scope.sc.stream.concat.apply($scope.sc.stream, activities.collection);
     $scope.sc.getEmbed();
   });
-
-  $scope.sc.getEmbed = function getEmbed () {
-    for (var i = 0; i < $scope.sc.stream.length; i++) {
-      var uri = $scope.sc.stream[i].origin.uri;
-      SC.oEmbed(uri, { auto_play: false }, function(oEmbed) {
-        // console.log(JSON.stringify(oEmbed, null, 2));
-        this.origin.oEmbed = oEmbed;
-        this.origin.oEmbed.html = $sce.trustAsHtml(oEmbed.html);
-        // $scope.$apply();
-      }.bind($scope.sc.stream[i]));
-    }
-  };
 }]);
